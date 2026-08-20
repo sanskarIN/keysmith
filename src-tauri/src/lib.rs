bd6046d4015168a50114711fb6443010140cf56c
Sanskar <sanskarin@outlook.in>
2026-08-20 06:11:52 +0530

    feat: initialize clipboard plugin on desktop and mobile

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .invoke_handler(tauri::generate_handler![
             commands::generate_password_command,
             commands::generate_batch_command,
