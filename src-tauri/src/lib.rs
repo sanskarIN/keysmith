0aac4660e6a09cf5ab3fabfd853742b5a66460d2
Sanskar <sanskarin@outlook.in>
2026-08-20 06:12:48 +0530

    feat: initialize cross-platform export plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,8 @@ mod commands;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             commands::generate_password_command,
             commands::generate_batch_command,
